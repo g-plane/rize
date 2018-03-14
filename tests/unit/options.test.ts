3c564ee7a0a434817b9a3138675e85049069f888
Pig Fang <g-plane@hotmail.com>
2018-03-14 19:12:31 +0800

    test(options): add test for setting viewport option

--- tests/unit/options.test.ts
+++ tests/unit/options.test.ts
@@ -6,3 +6,16 @@ test('pass an existing puppeteer browser', async () => {
   const instance = new Rize({ browser })
   await instance.end()
 }, process.env.CI ? 8000 : 5000)
+
+test('set viewport', async () => {
+  expect.assertions(1)
+  const instance = new Rize({ width: 1920, height: 1080 })
+  await instance
+    .execute((browser, page) => {
+      expect(page.viewport()).toEqual(expect.objectContaining({
+         width: 1920,
+         height: 1080
+      }))
+    })
+    .end()
+})
